8b0394182ee6f46131262ccf4b6c4c925ff55d2e
akrherz <akrherz@iastate.edu>
2023-09-15 13:37:50 -0500

    enable windows tests
    closes #1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,12 +9,17 @@ cmake -G Ninja ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
+  -DMZ_BUILD_TESTS=ON ^
+  -DMZ_BUILD_UNIT_TESTS=ON ^
   -DBUILD_SHARED_LIBS=ON ^
   -DMZ_FORCE_FETCH_LIBS=OFF ^
   ..
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build .
+if %ERRORLEVEL% neq 0 exit 1
+
+ctest --output-on-failure
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --install .
