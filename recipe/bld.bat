3ec85e57823e7081339979261c1aca381807fe5c
akrherz <akrherz@iastate.edu>
2023-10-03 08:06:59 -0500

    build minizip 1.2

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,13 @@
 set CC=cl
 
-cmake -S . -B . ^
+cmake -S . -B build ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DCMAKE_INSTALL_LIBDIR="%LIBRARY_PREFIX%\lib" ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DBUILD_SHARED_LIBS=ON ^
+  -DUSE_AES=OFF ^
   -DMZ_FORCE_FETCH_LIBS=OFF
 
-cmake --build . --config Release
+cmake --build build --config Release
 
-cmake --install . --config Release
+cmake --install build --config Release
