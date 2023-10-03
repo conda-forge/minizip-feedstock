88948340d4517de29429d374311bf8013651e8c8
akrherz <akrherz@iastate.edu>
2023-10-03 13:39:26 -0500

    adjust bld.bat per local experiments

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,21 @@
 set CC=cl
 
-cmake -S . -B build ^
+cmake -S . -B build -G Ninja ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-  -DCMAKE_INSTALL_LIBDIR="%LIBRARY_PREFIX%\lib" ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DBUILD_SHARED_LIBS=ON ^
-  -DUSE_AES=OFF ^
-  -DMZ_FORCE_FETCH_LIBS=OFF
+  -DUSE_AES=OFF
 
-cmake --build build --config Release
+cd build
+ninja install
+cd ..
 
-cmake --install build --config Release
+cmake -S . -B sbuild -G Ninja ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DBUILD_SHARED_LIBS=OFF ^
+  -DUSE_AES=OFF
+
+cd sbuild
+ninja install
+cd ..
